c4a9a6035023102f144b4005dd232d1ff2e74f62
issackj1 <issack.john1@ucalgary.ca>
2018-12-01 21:27:08 -0700

    event page updated

--- ExploreYYCHighFidelity/ExploreYYCHighFidelity/obj/Debug/ExploreYYCHighFidelity_Content.g.i.cs
+++ ExploreYYCHighFidelity/ExploreYYCHighFidelity/obj/Debug/ExploreYYCHighFidelity_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("img/event.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("this.jpeg")]
 
 
