76d4785d76c164e9f430646a3bf62460d193dfa5
Kevin Huynh <kevinhuynh8@outlook.com>
2018-12-02 13:17:04 -0700

    IDontKnowWhatImDoing

--- ExploreYYCHighFidelity/ExploreYYCHighFidelity/Calendar/Calendar/MainWindow.xaml.cs
+++ ExploreYYCHighFidelity/ExploreYYCHighFidelity/Calendar/Calendar/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,21 @@ namespace Calendar
 	public MainWindow()
 	{
 	    InitializeComponent();
+	    Storyboard sbD = this.FindResource("DecSb") as Storyboard;
+	    sbD.Completed += OnStoryboardDec;
+	    Storyboard sbJ = this.FindResource("JanSb") as Storyboard;
+	    sbJ.Completed += OnStoryboardJan;
+	}
+
+	private void OnStoryboardDec(object sender, EventArgs e)
+	{
+	    JanuaryGrid.Visibility = Visibility.Collapsed;
+	    ScheduleGrid.Visibility = Visibility.Collapsed;
+	}
+	private void OnStoryboardJan(object sender, EventArgs e)
+	{
+	    JanuaryGrid.Visibility = Visibility.Collapsed;
+	    ScheduleGrid.Visibility = Visibility.Collapsed;
 	}
     }
 }
