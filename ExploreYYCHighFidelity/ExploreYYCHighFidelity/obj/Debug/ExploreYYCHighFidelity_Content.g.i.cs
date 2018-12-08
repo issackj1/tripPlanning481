643e937b9143b1bb998d02618091c69fe71ef41c
issackj1 <issack.john1@ucalgary.ca>
2018-12-08 13:33:51 -0700

    Added pages
    Three new event pages and the blank events page

--- ExploreYYCHighFidelity/ExploreYYCHighFidelity/obj/Debug/ExploreYYCHighFidelity_Content.g.i.cs
+++ ExploreYYCHighFidelity/ExploreYYCHighFidelity/obj/Debug/ExploreYYCHighFidelity_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("tower2.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("img/jcbsnutcracker.jpg")]
 
 
